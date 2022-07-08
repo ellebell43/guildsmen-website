f0876c6c45b8622af27b1e1016d2c1bbb05a39c0
Aaron Rutherford <rutherfordaaron1@gmail.com>
2022-07-07 22:56:11 -0700

    Added in all people creatures plus some fixes
    - Improved footer appearance on both mobile and desktop
    - Made the back button a custom HTML element for easier changeability and useability
    - The back button is now the same as going back a page on your browser

---
+++ static/js/Back.js
@@ -0,0 +1,15 @@
+class Back extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+    <div class='backToContents'>
+      <a href="javascript:history.back()"><img src="/static/icons/arrow-left-solid.svg" alt="" class="filter" />Back</a>
+    </div>
+    `
+  }
+}
+
+customElements.define('back-component', Back);
--- static/js/Footer.js
+++ static/js/Footer.js
@@ -7,19 +7,17 @@ class Footer extends HTMLElement {
     this.innerHTML = `
     <footer>
       <div class='footerSocialLinks'>
-      <a href='https://discord.gg/6zE4chEVQQ'><img src='/static/icons/discord-brands.svg' alt='Discord link' class='filter' /></a>
-      <a href='https://www.reddit.com/r/Guildsmen/'><img src='/static/icons/reddit-brands.svg' alt='Reddit link' class='filter' /></a>
-
+        <a href='https://discord.gg/6zE4chEVQQ'><img src='/static/icons/discord-brands.svg' alt='Discord link' class='filter' /></a>
+        <a href='https://www.reddit.com/r/Guildsmen/'><img src='/static/icons/reddit-brands.svg' alt='Reddit link' class='filter' /></a>
       </div>
-        <div class='footerInfo'>
-          <div>
-            <p>Created by Aaron Rutherford and Harmony Brooks</p>
-            <p>Coded by <a href="https://github.com/hazipan">Aaron Rutherford</a></p>
-        </div>
-        <div class='rightDiv'>
-          <p>&copy; 2022 Aaron Rutherford</p>
-          <p>License - <a href='https://opensource.org/licenses/GPL-3.0'>GPL-3.0</a></p>
-        </div>
+
+      <div class='leftDiv'>
+        <p>Created by Aaron Rutherford<br class='break'>and Harmony Brooks</p>
+        <p>Coded by <a href="https://github.com/hazipan">Aaron Rutherford</a></p>
+      </div>
+      <div class='rightDiv'>
+        <p>&copy; 2022 Aaron Rutherford</p>
+        <p>License - <a href='https://opensource.org/licenses/GPL-3.0'>GPL-3.0</a></p>
       </div>
     </footer>
     `
