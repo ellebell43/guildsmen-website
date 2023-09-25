8db5cd67f5bb14de2238fc7b535d1cdde2fa7074
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-09-25 08:40:08 -0700

    Metadata for privacy policy, sign in, and profile

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,4 +1,9 @@
 import Link from "next/link";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Guildsmen | Privacy Policy"
+}
 
 export default function PrivacyPolicy() {
   return (
