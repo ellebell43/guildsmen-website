2fcbe1e0936998386118e289341e52e16efd3680
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-11-01 10:08:20 -0700

    Create dice-roll-wrapper.tsx
    - Wrapper component that adds a dice roll button for use on stats, skills, etc

---
+++ util/components/dice/dice-roll-wrapper.tsx
@@ -0,0 +1,53 @@
+import { faDice } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { rollDie } from "./die";
+
+export default function DiceRollWrapper(props: { children: React.ReactNode, mod?: number, modLabel?: string, setShowDice: Function, die1ID: string, die2ID: string, setRollMessage: Function }) {
+
+  const getModifier = () => {
+    if (props.mod) {
+      if (props.mod >= 0) return `+${props.mod}`
+      return props.mod
+    }
+    return ""
+  }
+
+  const getModifierString = () => {
+    if (props.mod) {
+      if (props.mod >= 0) return ` with a ${getModifier()}`
+      return ` with a ${props.mod}`
+    }
+    return ""
+  }
+
+  const getModifierLabelString = () => {
+    if (props.modLabel) {
+      return ` from ${props.modLabel}`
+    }
+    return ""
+  }
+
+  return (
+    <div className="flex justify-center items-center gap-2">
+      <button
+        onClick={e => {
+          props.setShowDice(true)
+          setTimeout(() => {
+            let num1 = rollDie(props.die1ID)
+            let num2 = rollDie(props.die2ID)
+            let total = num1 + num2
+            if (props.mod) total += props.mod
+            props.setRollMessage(<>
+              <p className="text-center m-0">({num1}+{num2}){getModifier()}</p>
+              <p className="text-center font-bold text-[32px] m-0">{total}</p>
+              <p className="text-xs italic opacity-70 text-center m-0">You rolled {num1} and {num2}{getModifierString()}{getModifierLabelString()} for a total of {total}</p>
+            </>, 100)
+          })
+        }}
+      >
+        <FontAwesomeIcon icon={faDice} />
+      </button>
+      {props.children}
+    </div>
+  )
+}
