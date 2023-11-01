1410aa0d39b1e363a44f2fbe2bc1ec66e4d8255c
Aaron Rutherford <rutherfordaaron1@gmail.com>
2023-11-01 10:32:16 -0700

    Style and label change
    - Removed borders for a cleaner feel
    - Added labels underneath the icons for better UX

--- app/resources/character-app/page-footer.tsx
+++ app/resources/character-app/page-footer.tsx
@@ -2,27 +2,33 @@ import { faBagShopping, faBook, faCircleInfo, faGear, faNoteSticky, faPerson } f
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function PageFooter(props: { active: "character" | "skills" | "gear" | "details" | "notes" | "settings", setActive: Function }) {
-  const buttonClass = "border-stone-400 dark:border-stone-200 border-t-2 w-full py-2 hover:bg-stone-300 dark:hover:bg-stone-500 hover:cursor-pointer"
+  const buttonClass = "border-stone-400 dark:border-stone-200 w-full py-2 hover:bg-stone-300 dark:hover:bg-stone-500 hover:cursor-pointer"
   const iconClass = "text-[18px]"
   return (
     <div className="fixed inset-x-0 bottom-0 border-t bg-stone-200 dark:bg-stone-600 flex justify-evenly">
-      <button className={`${buttonClass} border-r`} onClick={e => props.setActive("character")}>
+      <button className={`${buttonClass}`} onClick={e => props.setActive("character")}>
         <FontAwesomeIcon className={iconClass} icon={faPerson} />
+        <p className="m-0 text-xs text-center">Stats</p>
       </button>
-      <button className={`${buttonClass} border-l border-r`} onClick={e => props.setActive("skills")}>
+      <button className={`${buttonClass}`} onClick={e => props.setActive("skills")}>
         <FontAwesomeIcon className={iconClass} icon={faBook} />
+        <p className="m-0 text-xs text-center">Skills</p>
       </button>
-      <button className={`${buttonClass} border-l border-r`} onClick={e => props.setActive("gear")}>
+      <button className={`${buttonClass}`} onClick={e => props.setActive("gear")}>
         <FontAwesomeIcon className={iconClass} icon={faBagShopping} />
+        <p className="m-0 text-xs text-center">Gear</p>
       </button>
-      <button className={`${buttonClass} border-l border-r`} onClick={e => props.setActive("details")}>
+      <button className={`${buttonClass}`} onClick={e => props.setActive("details")}>
         <FontAwesomeIcon className={iconClass} icon={faCircleInfo} />
+        <p className="m-0 text-xs text-center">Details</p>
       </button>
-      <button className={`${buttonClass} border-l border-r`} onClick={e => props.setActive("notes")}>
+      <button className={`${buttonClass}`} onClick={e => props.setActive("notes")}>
         <FontAwesomeIcon className={iconClass} icon={faNoteSticky} />
+        <p className="m-0 text-xs text-center">Notes</p>
       </button>
-      <button className={`${buttonClass} border-l`} onClick={e => props.setActive("settings")}>
+      <button className={`${buttonClass}`} onClick={e => props.setActive("settings")}>
         <FontAwesomeIcon className={iconClass} icon={faGear} />
+        <p className="m-0 text-xs text-center">Settings</p>
       </button>
     </div>
   )
