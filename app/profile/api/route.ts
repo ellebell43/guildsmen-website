4ea629f34976c791af8e2755ff995ac34eb2ae0a
Aaron Rutherford <rutherfordaaron1@gmail.com>
2024-01-18 13:22:43 -0800

    logging token to try to figure out missing token issue

--- app/profile/api/route.ts
+++ app/profile/api/route.ts
@@ -5,6 +5,7 @@ import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(req: NextRequest) {
   const token = req.cookies.get("token")
+  console.log(token)
   if (!token) return NextResponse.json({ message: "No user token provided" }, { status: 400 })
 
   if (req.headers.get("getAvatarUrl") === "true") {
