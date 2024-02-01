7b34a4cd287accdc520fff8bb677fdfea39425a6
Aaron Rutherford <rutherfordaaron1@gmail.com>
2024-02-01 12:50:02 -0800

    Better large screen layouts for character app

--- app/resources/character-app/gear-screen.tsx
+++ app/resources/character-app/gear-screen.tsx
@@ -1,9 +1,12 @@
 import EditableList from "@/util/components/editable-list";
 import { Character } from "@/util/types";
+import useWindowDimensions from "@/util/useWindowDimenstions";
 import { useEffect, useState } from "react";
 
-export default function GearScreen(props: { character: Character, setCharacter: Function }) {
+export default function GearScreen(props: { character: Character, setCharacter: Function, headerClass: string, containerClass: string }) {
   const [items, setItems] = useState([...props.character.gear])
+
+  const { width, height } = useWindowDimensions()
 
   useEffect(() => {
     let newChar = { ...props.character }
@@ -12,8 +15,9 @@ export default function GearScreen(props: { character: Character, setCharacter: 
   }, [items])
 
   return (
-    <>
+    <div className={`${props.containerClass} flex flex-col pl-8 mt-4 max-h-[700px] overflow-y-scroll`}>
+      <h2 className={`${props.headerClass} ${width >= 1280 ? "" : "hidden"}`}>Gear</h2>
       <EditableList state={items} setState={setItems} placeholder="New Item" />
-    </>
+    </div>
   )
 }
--- app/resources/character-app/page-footer.tsx
+++ app/resources/character-app/page-footer.tsx
@@ -1,20 +1,32 @@
+import useWindowDimensions from "@/util/useWindowDimenstions";
 import { faBagShopping, faBook, faCircleInfo, faGear, faNoteSticky, faPerson } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function PageFooter(props: { active: "character" | "skills" | "gear" | "details" | "notes" | "settings", setActive: Function }) {
-  const buttonClass = "border-stone-400 dark:border-stone-200 w-full py-2 hover:bg-stone-300 dark:hover:bg-stone-500 hover:cursor-pointer lg:h-full lg:flex lg:gap-2 lg:items-center lg:pl-2"
+  const buttonClass = "border-stone-400 dark:border-stone-200 w-full py-2 hover:bg-stone-300 dark:hover:bg-stone-500 hover:cursor-pointer lg:h-full lg:flex lg:gap-2 lg:items-center lg:px-2"
   const iconClass = "text-[18px] max-w-[20px]"
+
+  const { width, height } = useWindowDimensions()
+  let size: "sm" | "md" | "lg"
+  if (width <= 768) {
+    size = "sm"
+  } else if (width <= 1280) {
+    size = "md"
+  } else {
+    size = "lg"
+  }
+
   return (
-    <div className="fixed bottom-0 right-0 lg:right-auto lg:top-16 left-0 lg:w-[100px] border-t lg:border-r border-stone-800 dark:border-stone-200 bg-stone-200 dark:bg-stone-600 flex lg:flex-col justify-evenly transition-all">
+    <div className="fixed bottom-0 right-0 left-0 lg:right-auto border-t lg:border-r border-stone-800 dark:border-stone-200 bg-stone-200 dark:bg-stone-600 flex lg:flex-col justify-evenly transition-all">
       <button className={`${buttonClass}  ${props.active == "character" ? "bg-stone-300 dark:bg-stone-500" : ""}`} onClick={e => { props.setActive("character"); window.scrollTo(0, 0) }}>
         <FontAwesomeIcon className={iconClass} icon={faPerson} />
-        <p className="m-0 text-xs lg:text-base text-center">Stats</p>
+        <p className="m-0 text-xs lg:text-base text-center">{size != "sm" ? "Character" : "Stats"}</p>
       </button>
-      <button className={`${buttonClass} ${props.active == "skills" ? "bg-stone-300 dark:bg-stone-500" : ""}`} onClick={e => { props.setActive("skills"); window.scrollTo(0, 0) }}>
+      <button className={`${buttonClass} ${props.active == "skills" ? "bg-stone-300 dark:bg-stone-500" : ""} md:hidden lg:hidden`} onClick={e => { props.setActive("skills"); window.scrollTo(0, 0) }}>
         <FontAwesomeIcon className={iconClass} icon={faBook} />
         <p className="m-0 text-xs lg:text-base text-center">Skills</p>
       </button>
-      <button className={`${buttonClass} ${props.active == "gear" ? "bg-stone-300 dark:bg-stone-500" : ""}`} onClick={e => { props.setActive("gear"); window.scrollTo(0, 0) }}>
+      <button className={`${buttonClass} ${props.active == "gear" ? "bg-stone-300 dark:bg-stone-500" : ""} lg:hidden`} onClick={e => { props.setActive("gear"); window.scrollTo(0, 0) }}>
         <FontAwesomeIcon className={iconClass} icon={faBagShopping} />
         <p className="m-0 text-xs lg:text-base text-center">Gear</p>
       </button>
