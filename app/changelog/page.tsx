cef7c4305d7dbacfbc2688ebb63f914ade201ac0
Aaron Rutherford <ellebell43@proton.me>
2024-06-22 13:39:13 -0700

    character and est sheet download links

--- app/changelog/page.tsx
+++ app/changelog/page.tsx
@@ -23,7 +23,7 @@ export default function Changelog() {
             "Navidus crystals have more definite sizes now",
             "A new level of difficulty (Super Difficult) has been added to the Skills chapter",
             "All rules pages now have a table of contents pullout component for easier navigation",
-            ""
+            "Added character sheet and establishment sheet to the Resources page for easier access"
           ]}
         />
         <ChangelogEntry
--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,13 +1,27 @@
 import { linkClass } from "@/util/variables";
 import Link from "next/link";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Guildsmen | Resources",
+  description: "Quick access links to various Guildsmen tools and sheets."
+}
 
 export default function Page() {
   return (
     <>
       <h1>Resources</h1>
-      <Link className={linkClass} href="/resources/character-creator">
-        Character Creator
-      </Link>
+      <div className="flex flex-col gap-4">
+        <Link className={linkClass} href="/resources/character-creator">
+          Character Creator
+        </Link>
+        <Link className={linkClass} href="/cs-sheet.pdf" download="guildsmen-character-sheet" target="_blank">
+          Character Sheet
+        </Link>
+        <Link className={linkClass} href="/est-sheet.pdf" download="guildsmen-establishment-sheet" target="_blank">
+          Character Sheet
+        </Link>
+      </div>
     </>
   )
 }
