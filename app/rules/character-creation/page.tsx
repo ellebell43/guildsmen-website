f3e6be30d97e7ed265cc7ce0cec97e5b41a15c47
Aaron Rutherford <ellebell43@proton.me>
2024-09-26 17:22:01 -0700

    GuildDetails component
    - same concept as SpeciesDetails to create one source of truth for guild details.
    - Also changed layout to grid for Medium+ screens to utilize space better.

--- app/rules/character-creation/page.tsx
+++ app/rules/character-creation/page.tsx
@@ -6,6 +6,7 @@ import { Metadata } from "next";
 import Contents from "@/util/components/contents";
 import Image from "next/image";
 import SpeciesDetails from "./species-details";
+import GuildDetails from "./guild-details";
 
 export const metadata: Metadata = {
   title: "Guildsmen | Character Creation",
@@ -177,130 +178,14 @@ export default function CharacterCreation() {
 
         <p>Once you choose a guild, all players will increase the skills listed to +1 if those skills are not already there.</p>
 
-        <h3>Assassins Guild</h3>
-        <p>Assassins are those that believe anyone is worthy of death… for the right price. Assassins lurk in the dark and strike when least expected. They are efficient killers and know how to get in, spill blood, and get out as quickly as possible.</p>
+        <div className="md:grid grid-cols-2 gap-4">
+          <GuildDetails guild="Assassins" />
+          <GuildDetails guild="Mythic Hunters" />
+          <GuildDetails guild="Explorers" />
+          <GuildDetails guild="Mercenaries" />
+          <GuildDetails guild="Thieves" />
+        </div>
 
-        <table className="raceStatTable">
-          <thead>
-            <tr>
-              <th colSpan={1}>Guild Skills</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="bottom left right"><em>Investigate</em></td>
-            </tr>
-            <tr>
-              <td className="bottom left right"><em>Sneaky</em></td>
-            </tr>
-            <tr>
-              <td className="bottom left right"><em>Social</em></td>
-            </tr>
-            <tr>
-              <td className="bottom left right"><em>Throwdown</em></td>
-            </tr>
-          </tbody>
-        </table>
-
-        <h3>Mythic Hunters Guild</h3>
-        <p>Mythic Hunters are those that thrill in hunting the most dangerous beasts of the wilds. Their main goal is to kill Mythical creatures, harvest Navidus Crystals from them, and bring the crystals back to the guild. Occasionally, they will get hired to kill Mythical creatures that are causing a nuisance, or to help defend smaller towns from Mythical creature attacks.</p>
-
-        <table className="raceStatTable">
-          <thead>
-            <tr>
-              <th colSpan={1}>Guild Skills</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="bottom left right"><em>Investigate</em></td>
-            </tr>
-            <tr>
-              <td className="bottom left right"><em>Medic</em></td>
-            </tr>
-            <tr>
-              <td className="bottom left right"><em>Nature</em></td>
-            </tr>
-            <tr>
-              <td className="bottom left right"><em>Throwdown</em></td>
-            </tr>
-          </tbody>
-        </table>
-
-        <h3>Explorers Guild</h3>
-        <p>Explorers desire to bring the unknown world into the light and to solve the mysteries of the world. They go beyond the borders of the map to discover lands that no one has ever experienced and they know that the answers to the worlds greatest questions can be found, if you can only look in the right spot.</p>
-
-        <table className="raceStatTable">
-          <thead>
-            <tr>
-              <th colSpan={1}>Guild Skills</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="bottom left right"><em>Craft</em></td>
-            </tr>
-            <tr>
-              <td className="bottom left right"><em>Investigate</em></td>
-            </tr>
-            <tr>
-              <td className="bottom left right"><em>Medic</em></td>
-            </tr>
-            <tr>
-              <td className="bottom left right"><em>Throwdown</em></td>
-            </tr>
-          </tbody>
-        </table>
-
-        <h3>Mercenaries Guild</h3>
-        <p>Mercenaries are those who use their strength to make a profit. They are loyal defenders – if they get paid. They are masters of martial skills and make formidable opponents.</p>
-
-        <table className="raceStatTable">
-          <thead>
-            <tr>
-              <th colSpan={1}>Guild Skills</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="bottom left right"><em>Craft</em></td>
-            </tr>
-            <tr>
-              <td className="bottom left right"><em>Investigate</em></td>
-            </tr>
-            <tr>
-              <td className="bottom left right"><em>Medic</em></td>
-            </tr>
-            <tr>
-              <td className="bottom left right"><em>Throwdown</em></td>
-            </tr>
-          </tbody>
-        </table>
-
-        <h3>Thieves Guild</h3>
-        <p>Thieves understand the concept of value. They know that a fortune can only be made when working outside the law. Nothing is beyond their grasp and no place is prohibited to them – if they can be sneaky enough.</p>
-
-        <table className="raceStatTable">
-          <thead>
-            <tr>
-              <th colSpan={1}>Guild Skills</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="bottom left right"><em>Investigate</em></td>
-            </tr>
-            <tr>
-              <td className="bottom left right"><em>Performance</em></td>
-            </tr>
-            <tr>
-              <td className="bottom left right"><em>Sneaky</em></td>
-            </tr>
-            <tr>
-              <td className="bottom left right"><em>Throwdown</em></td>
-            </tr>
-          </tbody>
-        </table>
       </section>
       <section id="going-forward">
         <h2>Going Forward</h2>
