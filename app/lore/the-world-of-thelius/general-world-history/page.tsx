c7e26c099f464f54d3d11251dc2e493297dbb5b8
Elle Brooks <ellebell43@proton.me>
2025-01-16 11:42:39 -0800

    Overview completetion
    - Mythical is changed to Starborne
    - Myth is changed to stardew
    - Mythic Crisis is changed to Starfall
    - General editing and filling out of thoughts

--- app/lore/the-world-of-thelius/general-world-history/page.tsx
+++ app/lore/the-world-of-thelius/general-world-history/page.tsx
@@ -19,40 +19,34 @@ export default function GeneralWorldHistory() {
 
         <p>This cloud, known as the Star Storm, moved across the land and everywhere it touched, chaos would unfold. Everything it touched would warp and waver and reality would change. The rules that governed the universe would cease to hold sway where ever the storm found purchase. Entire civilizations were wiped off the face of the world in a matter of months.</p>
 
-        <p>After a few desperate years of this horrific storm sweeping around the land, new creatures appeared. These Mythic creatures appeared to have been born of the Star Storm because they fed and thrived off of its energy. New plants, herbivores, and predators created an entire ecosystem around the energy of the Storm that began to thrive. Their population exploded rapidly, and as they fed off the energy of the storm, it got smaller, and smaller.</p>
+        <p>After a few desperate years of this horrific storm sweeping around the world, new creatures appeared. These Starborne creatures appeared to have been born of the Star Storm because they fed and thrived off of its energy. New plants, herbivores, and predators created an entire ecosystem around the energy of the Storm that began to thrive. Their population exploded rapidly, and as they fed off the energy of the storm, it got smaller, and smaller. Eventually the storm ceased to be a problem, but a new problem presented itself right away.</p>
 
-        {/* <p>However, the overall prosperity didn't last. It started as a few ordinary creatures, generally predators, exhibiting strange powers; manipulation of the elements. And with their new powers, these few creatures also became ravenous, willing to eat anything made of meat - people included. At first, it was mostly seen as a few frightening cases. Odd but not overall worrisome. Nonetheless, people became much more frightened of wild animals, but no real panic was created. However, over the course of about 20 years, the aggressiveness of these creatures spread completely throughout their own species and the amount of Mythical creatures in the world increased dramatically. Entire towns began to be attacked and were destroyed. People fled to large cities in the hope of protection against the threat of these mythical creatures, but even cities began to fall. People soon found that when attacked head on, these new Mythical creatures could explode violently when attacked, and no one knew why. No one could fight them and live. The few that tried ended up causing massive explosions that tore the earth apart as they accidentally broke the Navidus crystals that every Mythical animal has since no one at the time knew about them.</p>
+        <p>Not only was the entire world reshaped, but now it was filled primarily with Starborne creatures and their primary source of food was dwindling rapidly, so they were turing to anything else they could eat. That primarily meant each other, but also meant every other creature near by. What few people remained were nearly wiped out by these ravenous creatures until many of them died out.</p>
 
-        <p>With city after city being toppled by Mythical animals, mass panic, and a lack of resources, nations began to collapse. So, hundreds of expeditionary groups were sent out to try and find new places to live that either had no Mythical creatures or at least had strong enough natural protections to give them the advantage they needed to survive. Almost all the groups failed, but six of them managed to find a place that provided some safety from the mythical creatures. These six cities became the Ancestor City-States: Aquina, Arbor, Corden, Inidom, Kulasis, and Galus.</p> */}
+        <p>After the 200 hundred years containing Starfall, the Star Storm, and consequent Starborne Creature explosion, the world settled into an uneasy quiet. The Star Storm was gone, the Starfall Creatures were adapting to a world without it, and the sophonts of Thelius began to rebuild. Each of the Thelisian species founded a city-state which are now known as the Ancestor City-States. They were strongholds built during the Starborne Explosion where they could survive and weather the changing world.</p>
 
-        <p>And that's how the world found itself, and it stayed that way for about 500 years. During this time, each of the Ancestor City-States rebuilt their culture, discovered new ways of defending themselves from their hostile environments, and more-or-less prospered. These cities were inhabited by only one species each and over time the knowledge of the other species faded into legend. Nearly all documentation of the time before the Mythic Crisis was lost except for a few maps and books, and for each of the species the world became very, very small. Then, the airship was invented.</p>
+        <p>And that's how the world found itself, and it stayed that way for about 500 years. During this time, each of the Ancestor City-States rebuilt their culture, discovered new ways of defending themselves from their hostile environments, and more-or-less prospered. Each of these cities were inhabited by only one species, and over time the knowledge of the other species faded into legend. Nearly all documentation of the time before the Starfall was lost except for a few maps and books, and for each of the species the world became very, very small. Then, the airship was invented.</p>
 
-        <p>Around 500 years after the founding of the Ancestor City-States, the first airship was constructed in Kulasis, the desert city of the Slated, and the world began to be explored again. The Slated were desperate to escape their desert city, since the environment there was incredibly unforgiving. Some of the biggest Mythical creatures burrow beneath the sands, sandstorms are frequent and intense, and food is very difficult to grow. The Slated's entire culture was built around progress and innovation with the goal of finding a new home. With airships at their disposal, they found one.</p>
+        <p>Around 500 years after the founding of the Ancestor City-States, the first airship was constructed in Kulasis, the desert city of the Slated, and the world began to be explored in ernest again. The Slated were desperate to escape their desert city, since the environment there was incredibly unforgiving. Some of the biggest Starborne creatures burrow beneath the sands, sandstorms are frequent and intense, and food is very difficult to grow. The Slated's entire culture was built around progress and innovation with the goal of finding a new home. With airships at their disposal, they found one.</p>
 
-        <p>At the center of a large area of meadows, grass, and meandering streams rises a singular and great plateau, and it was here that the Slated founded their new city: Avidal, the City of Progress. Over the course of the next 50 years, they constructed their new home, continued exploring the world, and never lost their sense of innovation. Over time, they re-discovered the other sentient species of the world and invited them to help in the construction of Avidal, to make it a hub of ideas, cultures, and inclusion.</p>
+        <p>At the center of a large area of meadows, grass, and meandering streams rises a singular and great plateau, and it was here that the Slated founded their new city: Avidal, the City of Progress. Over the course of the next 50 years, they constructed their new home, continued exploring the world, and never lost their sense of innovation. Over time, they re-discovered the other peoples of the world and invited them to help in the construction and expansion of Avidal, to make it a hub of ideas, cultures, and inclusion.</p>
 
-        <p>The world got bigger as new methods of communication were invented such as long distance radio. Airships allowed for trade and transportation to the various Ancestor City-States. It also became apparent that the mythical creature population had lessened enough to create small settlements throughout the land. Guilds became a standard business practice for groups of skilled workers to join together to create a larger business. And for 50 more years, the world grew and prospered.</p>
+        <p>The world continued to get bigger as new methods of communication were invented such as long distance radio, allowing rapid communication between the far flung cty-states and the new settlements that began to spring up with the realization that the Starborne populations had declined immensely. Airships allowed for trade and transportation to the various Ancestor City-States allowing the exchange of goods, knowledge, and labor. Guilds became a standard business practice for groups of skilled workers to join together to create groups of protected labor, and for 50 more years the world grew and prospered.</p>
 
-        <p>As the world currently stands, industry and engineering is picking up. Mythical creatures are being studied and understood. The great Mythologist, Heinris Navidus discovered ways to utilize the power of Mythical creatures as an energy source. The drug known as Myth is growing in popularity even though it is a crime to use or possess it. Centrifuge Technology is just beginning to emerge. And there is still so much wilderness to be explored, and so much knowledge about the times before the Mythic Crisis to be discovered.</p>
+        <p>As the world currently stands: industry and engineering is picking up; Starborne creatures are being studied and understood, but also hunted for their Star Gems; the great Starborne-ologist, Heinris Navidus, has discovered ways to utilize the power of Star Gems as an energy source; The drug known as Stardew is growing in popularity even though it is a crime to use or possess it in most places; Centrifuge Technology is just beginning to emerge; and there is still so much wilderness to be explored, and so much knowledge about the times before the Starfall to be discovered.</p>
       </section>
 
-      <section id="the-world-before">
-        <h2>The World Before</h2>
+      {/* <section id="the-world-before-starfall">
+        <h2>The World Before Starfall</h2>
         <p>The various cultures and religions of Thelius hold a wide variety of beliefs about the times before the Mythic Crisis, but none of them have the whole picture and most of them have lost so much history. What history they have managed to hold on to has become greatly misconstrued with legends and mystery. While each culture and religion has their own ideas of the times before the Mythic Crisis, the following is an accurate account.</p>
 
         <p>Before the Mythic Crisis, the world was much like our own world during its Renaissance Era. It was a major time of change for art, philosophy, political theory, economics and more. The continent was generally explored and maps had been made. The sophonts of Thelius mingled and warred, and the known world was slowly approaching a modern era.</p>
 
-        <p>During this time, mythical creatures didn't exist and magic took the form of simple parlor tricks and sleight of hand. Dragons were novelty creatures that were no bigger than a dog in size and the various sophonts were at the top of the food chain. They had very little to worry about other than the problems that they made for themselves.</p>
+        <p>During this time, Starborne creatures didn't exist and magic took the form of simple parlor tricks and sleight of hand. Dragons were novelty creatures that were no bigger than a dog in size and the various sophonts were at the top of the food chain. They had very little to worry about other than the problems that they made for themselves.</p>
       </section>
 
-      <section id="the-mythic-crisis">
-        <h2>The Mythic Crisis</h2>
-        {/* 
-        - Navidus energy originates from one very specific plant in one very specific place
-        - Normally these plants are only eaten by dragons (the original, singular mythical creature )
-          - Dragons are migratory, small animals that only eat this one plant
-        - Displaced prey species ate these plants and become unnatural mythic creatures
-          - Mythic creatures must eat to maintain their body or the navidus crystal out-paces their body and kills them */}
+      <section id="the-starfall">
+        <h2>The Starfall</h2>
       </section>
 
       <section>
@@ -61,21 +55,20 @@ export default function GeneralWorldHistory() {
 
       <section id="navidus-energy">
         <h2>Navidus Energy</h2>
-        {/* Most mythic creatures a very large since they can survive eating worse things for them that the Navidus energy parasite forces them to eat */}
       </section>
 
-      <section id="the-use-of-myth">
-        <h2>The Use of Myth</h2>
-        {/* 
-        - using unrefined myth causes an individual to become a mythic person
+      <section id="the-use-of-stardew">
+        <h2>The Use of Stardew</h2>
+        
+        - using unrefined stardew causes an individual to become a starborne person
           - Essentially a ravenous cannibal (they eat anything) with terrifying powers 
           - Zombies but better
-          - Some sophonts should get sick from eating their own kind (or raw meats in general), and they die from illness */}
+          - Some sophonts should get sick from eating their own kind (or raw meats in general), and they die from illness
       </section>
 
       <section id="centrifuge-technology">
         <h2>Centrifuge Technology</h2>
-      </section>
+      </section> */}
     </article>
   )
 }
