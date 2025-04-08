226651864e0e5962e392f3a02c2c732364714f0e
Elle Brooks <ellebell43@proton.me>
2025-04-07 18:39:57 -0700

    Info button for switches
    The edit and public switch now have info button to tell the user what they do

--- app/resources/character-app/settings.tsx
+++ app/resources/character-app/settings.tsx
@@ -1,4 +1,5 @@
 import Switch from "@/util/components/switch";
+import { Info } from "@/util/input-components/input-elements";
 import { Character } from "@/util/types";
 import Link from "next/link";
 
@@ -9,8 +10,14 @@ export default function Settings(props: { setEdit: Function, edit: boolean, char
 
   return (
     <div className="flex justify-center flex-col w-fit gap-y-4 mx-auto">
-      <Switch attribute={edit} setAttribute={setEdit} label="Edit Mode" />
-      <Switch attribute={character.public} setAttribute={setCharacter} attributeIsForObject={true} object={character} attributeTag="public" label="Public" />
+      <div className="relative">
+        <Info><p>Toggling this on allows you to adjust all the details of your character: name, skill levels, wealth, etc.</p></Info>
+        <Switch attribute={edit} setAttribute={setEdit} label="Edit Mode" />
+      </div>
+      <div className="relative">
+        <Info><p>Toggling this on allows other players to view your character (but not change it) if they have a link to it.</p></Info>
+        <Switch attribute={character.public} setAttribute={setCharacter} attributeIsForObject={true} object={character} attributeTag="public" label="Public" />
+      </div>
       <Link href={`/resources/character-sheet/${character._id}`} className="button px-6 py-2">Print View</Link>
     </div>
   )
