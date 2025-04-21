4856763d160b7c80e65d8fbdd6065f1040ef6b3c
Elle Brooks <ellebell43@proton.me>
2025-04-20 21:33:09 -0700

    test ii

--- app/resources/community-templates/page.tsx
+++ app/resources/community-templates/page.tsx
@@ -8,12 +8,14 @@ export default async function Page() {
   let list: characterTemplate[]
   const apiRoute = `${process.env.NEXT_PUBLIC_HOST}/resources/community-templates/api`
   const res = await fetch(apiRoute, { cache: "no-store", method: "GET" })
-  if (!res.ok) return <p>Error: something went wrong communicating with the server. Please refresh and try again.</p>
-  list = await res.json()
-
-  return (
-    <>
-      <CommunityTemplatesClient list={list} />
-    </>
-  )
+  if (!res.ok) {
+    return <p>Error: something went wrong communicating with the server. Please refresh and try again.</p>
+  } else {
+    list = await res.json()
+    return (
+      <>
+        <CommunityTemplatesClient list={list} />
+      </>
+    )
+  }
 }
