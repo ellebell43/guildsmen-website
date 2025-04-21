69211509c761fba321c029125ef52a6a3d8e717f
Elle Brooks <ellebell43@proton.me>
2025-04-20 21:49:59 -0700

    test v

--- app/resources/community-templates/page.tsx
+++ app/resources/community-templates/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = { title: "Guildsmen | Community Templates" }
 export default async function Page() {
   let list: characterTemplate[]
   const apiRoute = `${process.env.NEXT_PUBLIC_HOST}/resources/community-templates/api`
-  const res = await fetch(apiRoute, { cache: "no-store", method: "GET", credentials: "include" })
+  const res = await fetch(apiRoute, { cache: "no-store", method: "GET", credentials: "include", headers: { random: JSON.stringify(new Date()) } })
   if (!res.ok) {
     return <p>Error: something went wrong communicating with the server. Please refresh and try again.</p>
   } else {
