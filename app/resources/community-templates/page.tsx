b04b50006d6f1854463bd8bc2fcd798e46a5cb49
Elle Brooks <ellebell43@proton.me>
2025-04-20 21:57:50 -0700

    test vii

--- app/resources/community-templates/page.tsx
+++ app/resources/community-templates/page.tsx
@@ -2,12 +2,12 @@ import { Metadata } from "next";
 import CommunityTemplatesClient from "./client";
 import { characterTemplate } from "@/util/types";
 
-export const dynamic = 'force-dynamic'
+export const fetchCache = "force-no-store"
 export const metadata: Metadata = { title: "Guildsmen | Community Templates" }
 
 export default async function Page() {
   let list: characterTemplate[]
-  const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/resources/community-templates/api`, { cache: "no-store", method: "GET", next: { revalidate: 0 } })
+  const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/resources/community-templates/api`, { cache: "no-store", method: "GET" })
   if (!res.ok) {
     return <p>Error: something went wrong communicating with the server. Please refresh and try again.</p>
   } else {
