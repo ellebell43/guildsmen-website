ebcef1d8d616b637f292818c4d7884c0d1a19bb4
Elle Brooks <ellebell43@proton.me>
2025-04-20 22:00:56 -0700

    test viii

--- app/resources/community-templates/page.tsx
+++ app/resources/community-templates/page.tsx
@@ -2,12 +2,11 @@ import { Metadata } from "next";
 import CommunityTemplatesClient from "./client";
 import { characterTemplate } from "@/util/types";
 
-export const fetchCache = "force-no-store"
 export const metadata: Metadata = { title: "Guildsmen | Community Templates" }
 
 export default async function Page() {
   let list: characterTemplate[]
-  const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/resources/community-templates/api`, { cache: "no-store", method: "GET" })
+  const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/resources/community-templates/api?t=${JSON.stringify(new Date())}`, { cache: "no-store", method: "GET" })
   if (!res.ok) {
     return <p>Error: something went wrong communicating with the server. Please refresh and try again.</p>
   } else {
