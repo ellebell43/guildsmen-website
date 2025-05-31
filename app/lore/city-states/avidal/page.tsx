6b19693dbe342dd83938ee8df932a58b86e336fe
Elle Brooks <ellebell43@proton.me>
2025-05-31 07:26:10 -0700

    Avidal

---
+++ app/lore/city-states/avidal/page.tsx
@@ -0,0 +1,75 @@
+import Contents from "@/util/components/contents"
+
+export default function Page() {
+  return (
+    <article>
+      <Contents />
+      <h1>Avidal - City of Progress</h1>
+
+      <section id="overview">
+        <h2>Overview</h2>
+        Avidal is the center of the known world in many ways: geographically, economically, and politically. It sits on the Avidal Plateau at the center of the Nesarin Flatlands and serves as a hub for everything: nearly all goods flow from, to, or through Avidal; the Academy serves as the worlds center for knowledge and research; most of the worlds industrial factories and processing facilities sit next to Avidal's skyport; and Avidal's neighborhoods have the most cultural variety of any other city.
+
+        <h3>Bastion of Engineering</h3>
+        Avidal sets the golden standard for engineering and invention throughout the settled world of Thelius.
+        {/* - Public transportation via light rails/trams (do research)
+            - Mass production, guilds of inventors and others, education from the academy
+            - Reliance on the other city-states, their reliance on Avidal */}
+        <h3>World Hub</h3>
+        {/* World economics */}
+        <h3>The Council and the Atriarchs</h3>
+        {/* Various councils to handle details of city aspects, a couple atriarchs that oversee the councils */}
+        <h3>A Mask of Prosperity</h3>
+        {/* City is growing too fast, lowest of low town is a slum */}
+      </section>
+
+
+      <section id="the-founding-of-avidal">
+        {/* The exodus airship was named The Avidal */}
+        {/* Foundation Declaration was signed and passes by the Avidal crew on Olday, Avis 14, 512 PS*/}
+        <h2>The Founding of Avidal</h2>
+        Avidal was founded in 512 PS (Post Starfall), making the city roughly 140 years old, by a crew of Slated commanded by an individual name Nesarin.
+        <h3>The Desert Wastes</h3>
+        <h3>Exodus from Kulasis</h3>
+        <h3>The Nesarin Flatlands<br />and Avidal Plateau</h3>
+        <h3>The Building of the Council House</h3>
+        <h3>The Excavation of Underburrow</h3>
+      </section>
+
+
+      <section id="high-town">
+        <h2>High Town</h2>
+        {/* Also known as Old Town */}
+        <h3>Guild Court</h3>
+        <h4>The Institute of Applied History</h4>
+        <h4>The Inventor's Conclave</h4>
+        <h4>Bulwark Protectorate</h4>
+        <h4>Starfall Lodge</h4>
+
+        <h3>Council House</h3>
+
+        <h3>The Academy</h3>
+      </section>
+
+
+      <section id="low-town">
+        <h2>Low Town</h2>
+      </section>
+
+
+      <section id="the-grand-bazaar">
+        <h2>The Grand Bazaar</h2>
+      </section>
+
+
+      <section id="the-skyport">
+        <h2>The Skyport</h2>
+      </section>
+
+
+      <section id="underburrow">
+        <h2>Underburrow</h2>
+      </section>
+    </article>
+  )
+}
